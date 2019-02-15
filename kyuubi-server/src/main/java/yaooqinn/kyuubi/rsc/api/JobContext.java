586b26c1d4d2094f2ae1ae6f3a6b079ed3e25d02
hustfeiwang <wangfei3@corp.netease.com>
2019-02-15 18:56:44 +0800

    cp rscClinet

---
+++ kyuubi-server/src/main/java/yaooqinn/kyuubi/rsc/api/JobContext.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package yaooqinn.kyuubi.rsc.api;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.hive.HiveContext;
+
+/**
+ * Holds runtime information about the job execution context.
+ *
+ * An instance of this class is kept on the node hosting a remote Spark context and is made
+ * available to jobs being executed via RemoteSparkContext#submit().
+ */
+public interface JobContext {
+
+  /** The shared SparkContext instance. */
+  JavaSparkContext sc();
+
+  /** The shared SQLContext instance. */
+  SQLContext sqlctx();
+
+  /** The shared HiveContext instance. */
+  HiveContext hivectx();
+
+  /**
+   * Returns a local tmp dir specific to the context
+   */
+  File getLocalTmpDir();
+
+  /**
+   * Returns SparkSession if it existed, otherwise throws Exception.
+   */
+  <E> E sparkSession() throws Exception;
+}
---
+++ kyuubi-server/src/main/java/yaooqinn/kyuubi/rsc/driver/JobContextImpl.java
@@ -0,0 +1,124 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package yaooqinn.kyuubi.rsc.driver;
+
+import java.io.File;
+import java.lang.reflect.Method;
+
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaFutureAction;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.hive.HiveContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cloudera.livy.rsc.Utils;
+import yaooqinn.kyuubi.rsc.api.JobContext;
+
+class JobContextImpl implements JobContext {
+
+  private static final Logger LOG = LoggerFactory.getLogger(JobContextImpl.class);
+
+  private final JavaSparkContext sc;
+  private final File localTmpDir;
+  private volatile SQLContext sqlctx;
+  private volatile HiveContext hivectx;
+  private final RSCDriver driver;
+  private volatile Object sparksession;
+
+  public JobContextImpl(JavaSparkContext sc, File localTmpDir, RSCDriver driver) {
+    this.sc = sc;
+    this.localTmpDir = localTmpDir;
+    this.driver = driver;
+  }
+
+  @Override
+  public JavaSparkContext sc() {
+    return sc;
+  }
+
+  @Override
+  public Object sparkSession() throws Exception {
+    if (sparksession == null) {
+      synchronized (this) {
+        if (sparksession == null) {
+          try {
+            Class<?> clz = Class.forName("org.apache.spark.sql.SparkSession$");
+            Object spark = clz.getField("MODULE$").get(null);
+            Method m = clz.getMethod("builder");
+            Object builder = m.invoke(spark);
+            builder.getClass().getMethod("sparkContext", SparkContext.class)
+              .invoke(builder, sc.sc());
+            sparksession = builder.getClass().getMethod("getOrCreate").invoke(builder);
+          } catch (Exception e) {
+            LOG.warn("SparkSession is not supported", e);
+            throw e;
+          }
+        }
+      }
+    }
+
+    return sparksession;
+  }
+
+  @Override
+  public SQLContext sqlctx() {
+    if (sqlctx == null) {
+      synchronized (this) {
+        if (sqlctx == null) {
+          sqlctx = new SQLContext(sc);
+        }
+      }
+    }
+    return sqlctx;
+  }
+
+  @Override
+  public HiveContext hivectx() {
+    if (hivectx == null) {
+      synchronized (this) {
+        if (hivectx == null) {
+          hivectx = new HiveContext(sc.sc());
+        }
+      }
+    }
+    return hivectx;
+  }
+
+
+  @Override
+  public File getLocalTmpDir() {
+    return localTmpDir;
+  }
+
+  public synchronized void stop() {
+    if (sc != null) {
+      sc.stop();
+    }
+  }
+
+  public void addFile(String path) {
+    driver.addFile(path);
+  }
+
+  public void addJarOrPyFile(String path) throws Exception {
+    driver.addJarOrPyFile(path);
+  }
+}
