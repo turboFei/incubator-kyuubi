2b3efc5ea9105afbefd75cba0a086b131f0b6f95
Fei Wang <fwang12@ebay.com>
2022-04-24 00:08:23 +0800

    [KYUUBI #2448] Log the engine id when opening Kyuubi connection
    ### _Why are the changes needed?_
    
    Log the engine id when opening Kyuubi connection, it is more user friendly.
    
    ### _How was this patch tested?_
    - [ ] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [ ] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #2448 from turboFei/connection_engine_id.
    
    Closes #2448
    
    f05ffa19 [Fei Wang] log engine id
    
    Authored-by: Fei Wang <fwang12@ebay.com>
    Signed-off-by: Fei Wang <fwang12@ebay.com>

--- kyuubi-server/src/main/scala/org/apache/kyuubi/session/KyuubiSessionImpl.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/session/KyuubiSessionImpl.scala
@@ -152,7 +152,8 @@ class KyuubiSessionImpl(
             e)
           throw e
       }
-      logSessionInfo(s"Connected to engine [$host:$port] with ${_engineSessionHandle}")
+      logSessionInfo(s"Connected to engine [$host:$port]/[${client.engineId.getOrElse("")}]" +
+        s" with ${_engineSessionHandle}]")
       sessionEvent.openedTime = System.currentTimeMillis()
       sessionEvent.remoteSessionId = _engineSessionHandle.identifier.toString
       sessionCluster.foreach(sessionEvent.sessionCluster = _)
