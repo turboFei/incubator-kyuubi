d061aad9c169599c34737e8e614328cda3c4fb32
Fei Wang <fwang12@ebay.com>
2022-05-05 11:02:57 +0800

    [KYUUBI #2547] Support jdbc url prefix jdbc:kyuubi://
    ### _Why are the changes needed?_
    
    Support jdbc url prefix `jdbc:kyuubi://`
    ### _How was this patch tested?_
    - [x] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #2547 from turboFei/jdbc_kyuubi.
    
    Closes #2547
    
    608f46da [Fei Wang] fix ut
    cd267d3e [Fei Wang] add ut
    762631bb [Fei Wang] support jdbc:kyuubi://
    
    Authored-by: Fei Wang <fwang12@ebay.com>
    Signed-off-by: Fei Wang <fwang12@ebay.com>

--- kyuubi-server/src/test/scala/org/apache/kyuubi/operation/KyuubiOperationPerConnectionSuite.scala
+++ kyuubi-server/src/test/scala/org/apache/kyuubi/operation/KyuubiOperationPerConnectionSuite.scala
@@ -45,7 +45,9 @@ import org.apache.kyuubi.service.authentication.KyuubiAuthenticationFactory
  */
 class KyuubiOperationPerConnectionSuite extends WithKyuubiServer with HiveJDBCTestHelper {
 
-  override protected def jdbcUrl: String = getJdbcUrl
+  override protected def jdbcUrl: String =
+    s"jdbc:kyuubi://${server.frontendServices.head.connectionUrl}/;"
+  override protected val URL_PREFIX: String = "jdbc:kyuubi://"
 
   override protected val conf: KyuubiConf = {
     KyuubiConf().set(KyuubiConf.ENGINE_SHARE_LEVEL, "connection")
