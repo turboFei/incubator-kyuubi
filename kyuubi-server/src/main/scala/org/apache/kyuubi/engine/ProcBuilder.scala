0fe6c2a9fcfb062735b2f165236b385f71afbb08
Fei Wang <fwang12@ebay.com>
2022-06-08 10:31:50 +0800

    [KYUUBI #2373][SUB-TASK][KPIP-4] Support to recovery batch session on Kyuubi instances restart
    To close #2373
    
    - [x] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #2790 from turboFei/recovery_batch.
    
    Closes #2373
    
    81632191 [Fei Wang] trigger test
    6ac6f9bd [Fei Wang] async recovery
    6d9edf4e [Fei Wang] comments
    daa6719f [Fei Wang] refactor
    0c3a2e14 [Fei Wang] remove waitAppCompletion
    04e139ee [Fei Wang] comments
    b5d11d8f [Fei Wang] comment for method name
    321dfa95 [Fei Wang] refactor
    90be2df7 [Fei Wang] address comments
    14409582 [Fei Wang] batch recovery
    
    Authored-by: Fei Wang <fwang12@ebay.com>
    Signed-off-by: ulysses-you <ulyssesyou@apache.org>

--- kyuubi-server/src/main/scala/org/apache/kyuubi/engine/ProcBuilder.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/engine/ProcBuilder.scala
@@ -25,6 +25,7 @@ import java.util.UUID
 
 import scala.collection.JavaConverters._
 
+import com.google.common.annotations.VisibleForTesting
 import com.google.common.collect.EvictingQueue
 import org.apache.commons.lang3.StringUtils.containsIgnoreCase
 
@@ -181,6 +182,8 @@ trait ProcBuilder {
   @volatile private[kyuubi] var logCaptureThreadReleased: Boolean = true
   private var logCaptureThread: Thread = _
   private var process: Process = _
+  @VisibleForTesting
+  @volatile private[kyuubi] var processLaunched: Boolean = _
 
   private[kyuubi] lazy val engineLog: File = ProcBuilder.synchronized {
     val engineLogTimeout = conf.get(KyuubiConf.ENGINE_LOG_TIMEOUT)
@@ -220,6 +223,7 @@ trait ProcBuilder {
 
   final def start: Process = synchronized {
     process = processBuilder.start()
+    processLaunched = true
     val reader = Files.newBufferedReader(engineLog.toPath, StandardCharsets.UTF_8)
 
     val redirect: Runnable = { () =>
