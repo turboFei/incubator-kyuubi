5a376b26adaac6fe1f3991343540e5bbe3a45d9a
Cheng Pan <chengpan@apache.org>
2022-07-14 17:29:06 +0800

    [KYUUBI #3073] CredentialsManager should use appUser to renew credential
    CredentialsManager should use appUser to renew credentials instead of session user.
    
    - [ ] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #3073 from pan3793/renew.
    
    Closes #3073
    
    4938fb3e [Cheng Pan] CredentialsManager should use appUser to renew credential
    
    Authored-by: Cheng Pan <chengpan@apache.org>
    Signed-off-by: Cheng Pan <chengpan@apache.org>

--- kyuubi-server/src/main/scala/org/apache/kyuubi/session/KyuubiSessionImpl.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/session/KyuubiSessionImpl.scala
@@ -210,7 +210,7 @@ class KyuubiSessionImpl(
 
   private def renewEngineCredentials(): String = {
     try {
-      sessionManager.credentialsManager.renewCredentials(user, sessionCluster)
+      sessionManager.credentialsManager.renewCredentials(engine.appUser, sessionCluster)
     } catch {
       case e: Exception =>
         error(s"Failed to renew engine credentials for $handle", e)
