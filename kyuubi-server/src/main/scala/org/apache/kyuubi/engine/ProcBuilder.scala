14d24aa5bd5a97d2e27fa1edd9b65f148ba7afd2
wangjunbo <wangjunbo@qiyi.com>
2022-08-11 09:51:47 +0800

    [KYUUBI #833] Check if `spark.kubernetes.executor.podNamePrefix` is invalid
    ### _Why are the changes needed?_
    close #833
    
    ### _How was this patch tested?_
    - [ ] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #3164 from Kwafoor/master.
    
    Closes #833
    
    63995b5c [wangjunbo] [kyuubi-833]Check if spark.kubernetes.executor.podNamePrefix is invalid
    ef6663dd [wangjunbo] [kyuubi-833]Check if spark.kubernetes.executor.podNamePrefix is invalid
    
    Authored-by: wangjunbo <wangjunbo@qiyi.com>
    Signed-off-by: ulysses-you <ulyssesyou@apache.org>

--- kyuubi-server/src/main/scala/org/apache/kyuubi/engine/ProcBuilder.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/engine/ProcBuilder.scala
@@ -221,6 +221,8 @@ trait ProcBuilder {
     file
   }
 
+  def validateConf: Unit = {}
+
   final def start: Process = synchronized {
     process = processBuilder.start()
     processLaunched = true
