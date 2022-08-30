apache/kyuubi

[KYUUBI #3136] Change Map to a case class ApplicationInfo as the application info holder
### _Why are the changes needed?_

closes https://github.com/apache/incubator-kyuubi/issues/3136

### _How was this patch tested?_
- [ ] Add some test cases that check the changes thoroughly including negative and positive cases if possible

- [ ] Add screenshots for manual tests if appropriate

- [ ] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request

Closes #3137 from ulysses-you/application-info.

Closes #3136

d600414a [ulysses-you] style
89c68d06 [ulysses-you] style
f6b885f6 [ulysses-you] address comment
1f469c3e [ulysses-you] unknown
b0af99f8 [ulysses-you] unknown
6f1b67e3 [ulysses-you] address comment
1f9047e9 [ulysses-you] nit
e0a02e4b [ulysses-you] fix k8s
1ba0559d [ulysses-you] fix test
61788562 [ulysses-you] fix test
51eeca98 [ulysses-you] Merge branch 'master' of https://github.com/apache/incubator-kyuubi into application-info
c4871134 [ulysses-you] fix
7a876f06 [ulysses-you] fix
18524785 [ulysses-you] correct application state mapping
41234566 [ulysses-you] status compatibility
01f63852 [ulysses-you] Make application info stable

Authored-by: ulysses-you <ulyssesyou18@gmail.com>
Signed-off-by: ulysses-you <ulyssesyou@apache.org>

kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/v1/BatchesResource.scala
@@ -34,7 +34,7 @@ import org.apache.kyuubi.client.api.v1.dto._
 import org.apache.kyuubi.client.exception.KyuubiRestException
 import org.apache.kyuubi.config.KyuubiConf
 import org.apache.kyuubi.config.KyuubiReservedKeys.KYUUBI_CLIENT_IP_KEY
-import org.apache.kyuubi.engine.ApplicationOperation._
+import org.apache.kyuubi.engine.ApplicationInfo
 import org.apache.kyuubi.operation.{BatchJobSubmission, FetchOrientation, OperationState}
 import org.apache.kyuubi.server.api.ApiRequestContext
 import org.apache.kyuubi.server.api.v1.BatchesResource._
@@ -68,19 +68,19 @@ private[v1] class BatchesResource extends ApiRequestContext with Logging {
   private def buildBatch(session: KyuubiBatchSessionImpl): Batch = {
     val batchOp = session.batchJobSubmissionOp
     val batchOpStatus = batchOp.getStatus
-    val batchAppStatus = batchOp.currentApplicationState.getOrElse(Map.empty)
+    val batchAppStatus = batchOp.currentApplicationInfo
 
-    val name = Option(batchOp.batchName).getOrElse(batchAppStatus.get(APP_NAME_KEY).orNull)
+    val name = Option(batchOp.batchName).getOrElse(batchAppStatus.map(_.name).orNull)
     var appId: String = null
     var appUrl: String = null
     var appState: String = null
     var appDiagnostic: String = null
 
     if (batchAppStatus.nonEmpty) {
-      appId = batchAppStatus.get(APP_ID_KEY).orNull
-      appUrl = batchAppStatus.get(APP_URL_KEY).orNull
-      appState = batchAppStatus.get(APP_STATE_KEY).orNull
-      appDiagnostic = batchAppStatus.get(APP_ERROR_KEY).orNull
+      appId = batchAppStatus.get.id
+      appUrl = batchAppStatus.get.url.orNull
+      appState = batchAppStatus.get.state.toString
+      appDiagnostic = batchAppStatus.get.error.orNull
     } else {
       val metadata = sessionManager.getBatchMetadata(batchOp.batchId)
       appId = metadata.engineId
@@ -106,7 +106,7 @@ private[v1] class BatchesResource extends ApiRequestContext with Logging {
 
   private def buildBatch(
       metadata: Metadata,
-      batchAppStatus: Option[Map[String, String]]): Batch = {
+      batchAppStatus: Option[ApplicationInfo]): Batch = {
     batchAppStatus.map { appStatus =>
       val currentBatchState =
         if (BatchJobSubmission.applicationFailed(batchAppStatus)) {
@@ -119,11 +119,11 @@ private[v1] class BatchesResource extends ApiRequestContext with Logging {
           metadata.state
         }
 
-      val name = Option(metadata.requestName).getOrElse(appStatus.get(APP_NAME_KEY).orNull)
-      val appId = appStatus.get(APP_ID_KEY).orNull
-      val appUrl = appStatus.get(APP_URL_KEY).orNull
-      val appState = appStatus.get(APP_STATE_KEY).orNull
-      val appDiagnostic = appStatus.get(APP_ERROR_KEY).orNull
+      val name = Option(metadata.requestName).getOrElse(appStatus.name)
+      val appId = appStatus.id
+      val appUrl = appStatus.url.orNull
+      val appState = appStatus.state.toString
+      val appDiagnostic = appStatus.error.orNull
 
       new Batch(
         metadata.identifier,

kyuubi-server/src/test/scala/org/apache/kyuubi/WithKyuubiServerOnYarn.scala
@@ -23,6 +23,7 @@ import scala.concurrent.duration._
 import org.apache.kyuubi.config.KyuubiConf
 import org.apache.kyuubi.config.KyuubiConf._
 import org.apache.kyuubi.config.KyuubiConf.FrontendProtocols.FrontendProtocol
+import org.apache.kyuubi.engine.ApplicationState._
 import org.apache.kyuubi.engine.YarnApplicationOperation
 import org.apache.kyuubi.operation.{FetchOrientation, HiveJDBCTestHelper, OperationState}
 import org.apache.kyuubi.operation.OperationState.ERROR
@@ -116,9 +117,9 @@ class KyuubiOperationYarnClusterSuite extends WithKyuubiServerOnYarn with HiveJD
     val batchJobSubmissionOp = session.batchJobSubmissionOp
 
     eventually(timeout(3.minutes), interval(50.milliseconds)) {
-      val state = batchJobSubmissionOp.currentApplicationState
-      assert(state.nonEmpty)
-      assert(state.exists(_("id").startsWith("application_")))
+      val appInfo = batchJobSubmissionOp.currentApplicationInfo
+      assert(appInfo.nonEmpty)
+      assert(appInfo.exists(_.id.startsWith("application_")))
     }
 
     val killResponse = yarnOperation.killApplicationByTag(sessionHandle.identifier.toString)
@@ -127,7 +128,7 @@ class KyuubiOperationYarnClusterSuite extends WithKyuubiServerOnYarn with HiveJD
 
     val appInfo = yarnOperation.getApplicationInfoByTag(sessionHandle.identifier.toString)
 
-    assert(appInfo("state") === "KILLED")
+    assert(appInfo.state === KILLED)
 
     eventually(timeout(10.minutes), interval(50.milliseconds)) {
       assert(batchJobSubmissionOp.getStatus.state === ERROR)
@@ -145,12 +146,12 @@ class KyuubiOperationYarnClusterSuite extends WithKyuubiServerOnYarn with HiveJD
     val appUrl = rows("url")
     val appError = rows("error")
 
-    val state2 = batchJobSubmissionOp.currentApplicationState.get
-    assert(appId === state2("id"))
-    assert(appName === state2("name"))
-    assert(appState === state2("state"))
-    assert(appUrl === state2("url"))
-    assert(appError === state2("error"))
+    val appInfo2 = batchJobSubmissionOp.currentApplicationInfo.get
+    assert(appId === appInfo2.id)
+    assert(appName === appInfo2.name)
+    assert(appState === appInfo2.state.toString)
+    assert(appUrl === appInfo2.url.orNull)
+    assert(appError === appInfo2.error.orNull)
     sessionManager.closeSession(sessionHandle)
   }
 
@@ -168,7 +169,8 @@ class KyuubiOperationYarnClusterSuite extends WithKyuubiServerOnYarn with HiveJD
     val batchJobSubmissionOp = session.batchJobSubmissionOp
 
     eventually(timeout(3.minutes), interval(50.milliseconds)) {
-      assert(batchJobSubmissionOp.currentApplicationState.isEmpty)
+      assert(batchJobSubmissionOp.currentApplicationInfo.isDefined)
+      assert(batchJobSubmissionOp.currentApplicationInfo.get.id == null)
       assert(batchJobSubmissionOp.getStatus.state === OperationState.ERROR)
     }
   }

turboFei/incubator-kyuubi

kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/v1/BatchesResource.scala
@@ -34,7 +34,7 @@ import org.apache.kyuubi.client.api.v1.dto._
 import org.apache.kyuubi.client.exception.KyuubiRestException
 import org.apache.kyuubi.config.KyuubiConf
 import org.apache.kyuubi.config.KyuubiReservedKeys.KYUUBI_CLIENT_IP_KEY
-import org.apache.kyuubi.engine.ApplicationOperation._
+import org.apache.kyuubi.engine.ApplicationInfo
 import org.apache.kyuubi.operation.{BatchJobSubmission, FetchOrientation, OperationState}
 import org.apache.kyuubi.server.api.ApiRequestContext
 import org.apache.kyuubi.server.api.v1.BatchesResource._
@@ -68,19 +68,19 @@ private[v1] class BatchesResource extends ApiRequestContext with Logging {
   private def buildBatch(session: KyuubiBatchSessionImpl): Batch = {
     val batchOp = session.batchJobSubmissionOp
     val batchOpStatus = batchOp.getStatus
-    val batchAppStatus = batchOp.currentApplicationState.getOrElse(Map.empty)
+    val batchAppStatus = batchOp.currentApplicationState
 
-    val name = Option(batchOp.batchName).getOrElse(batchAppStatus.get(APP_NAME_KEY).orNull)
+    val name = Option(batchOp.batchName).getOrElse(batchAppStatus.map(_.name).orNull)
     var appId: String = null
     var appUrl: String = null
     var appState: String = null
     var appDiagnostic: String = null
 
     if (batchAppStatus.nonEmpty) {
-      appId = batchAppStatus.get(APP_ID_KEY).orNull
-      appUrl = batchAppStatus.get(APP_URL_KEY).orNull
-      appState = batchAppStatus.get(APP_STATE_KEY).orNull
-      appDiagnostic = batchAppStatus.get(APP_ERROR_KEY).orNull
+      appId = batchAppStatus.get.id
+      appUrl = batchAppStatus.get.url.orNull
+      appState = batchAppStatus.get.state.toString
+      appDiagnostic = batchAppStatus.get.error.orNull
     } else {
       val metadata = sessionManager.getBatchMetadata(batchOp.batchId)
       appId = metadata.engineId
@@ -107,7 +107,7 @@ private[v1] class BatchesResource extends ApiRequestContext with Logging {
 
   private def buildBatch(
       metadata: Metadata,
-      batchAppStatus: Option[Map[String, String]]): Batch = {
+      batchAppStatus: Option[ApplicationInfo]): Batch = {
     batchAppStatus.map { appStatus =>
       val currentBatchState =
         if (BatchJobSubmission.applicationFailed(batchAppStatus)) {
@@ -120,11 +120,11 @@ private[v1] class BatchesResource extends ApiRequestContext with Logging {
           metadata.state
         }
 
-      val name = Option(metadata.requestName).getOrElse(appStatus.get(APP_NAME_KEY).orNull)
-      val appId = appStatus.get(APP_ID_KEY).orNull
-      val appUrl = appStatus.get(APP_URL_KEY).orNull
-      val appState = appStatus.get(APP_STATE_KEY).orNull
-      val appDiagnostic = appStatus.get(APP_ERROR_KEY).orNull
+      val name = Option(metadata.requestName).getOrElse(appStatus.name)
+      val appId = appStatus.id
+      val appUrl = appStatus.url.orNull
+      val appState = appStatus.state.toString
+      val appDiagnostic = appStatus.error.orNull
 
       new Batch(
         metadata.identifier,

kyuubi-server/src/test/scala/org/apache/kyuubi/WithKyuubiServerOnYarn.scala
@@ -23,6 +23,7 @@ import scala.concurrent.duration._
 import org.apache.kyuubi.config.KyuubiConf
 import org.apache.kyuubi.config.KyuubiConf._
 import org.apache.kyuubi.config.KyuubiConf.FrontendProtocols.FrontendProtocol
+import org.apache.kyuubi.engine.ApplicationState._
 import org.apache.kyuubi.engine.YarnApplicationOperation
 import org.apache.kyuubi.operation.{FetchOrientation, HiveJDBCTestHelper, OperationState}
 import org.apache.kyuubi.operation.OperationState.ERROR
@@ -118,9 +119,9 @@ class KyuubiOperationYarnClusterSuite extends WithKyuubiServerOnYarn with HiveJD
     val batchJobSubmissionOp = session.batchJobSubmissionOp
 
     eventually(timeout(3.minutes), interval(50.milliseconds)) {
-      val state = batchJobSubmissionOp.currentApplicationState
-      assert(state.nonEmpty)
-      assert(state.exists(_("id").startsWith("application_")))
+      val appInfo = batchJobSubmissionOp.currentApplicationState
+      assert(appInfo.nonEmpty)
+      assert(appInfo.exists(_.id.startsWith("application_")))
     }
 
     val killResponse = yarnOperation.killApplicationByTag(sessionHandle.identifier.toString, None)
@@ -129,7 +130,7 @@ class KyuubiOperationYarnClusterSuite extends WithKyuubiServerOnYarn with HiveJD
 
     val appInfo = yarnOperation.getApplicationInfoByTag(sessionHandle.identifier.toString, None)
 
-    assert(appInfo("state") === "KILLED")
+    assert(appInfo.state === KILLED)
 
     eventually(timeout(10.minutes), interval(50.milliseconds)) {
       assert(batchJobSubmissionOp.getStatus.state === ERROR)
@@ -147,12 +148,12 @@ class KyuubiOperationYarnClusterSuite extends WithKyuubiServerOnYarn with HiveJD
     val appUrl = rows("url")
     val appError = rows("error")
 
-    val state2 = batchJobSubmissionOp.currentApplicationState.get
-    assert(appId === state2("id"))
-    assert(appName === state2("name"))
-    assert(appState === state2("state"))
-    assert(appUrl === state2("url"))
-    assert(appError === state2("error"))
+    val appInfo2 = batchJobSubmissionOp.currentApplicationState.get
+    assert(appId === appInfo2.id)
+    assert(appName === appInfo2.name)
+    assert(appState === appInfo2.state.toString)
+    assert(appUrl === appInfo2.url.orNull)
+    assert(appError === appInfo2.error.orNull)
     sessionManager.closeSession(sessionHandle)
   }
 
@@ -170,7 +171,8 @@ class KyuubiOperationYarnClusterSuite extends WithKyuubiServerOnYarn with HiveJD
     val batchJobSubmissionOp = session.batchJobSubmissionOp
 
     eventually(timeout(3.minutes), interval(50.milliseconds)) {
-      assert(batchJobSubmissionOp.currentApplicationState.isEmpty)
+      assert(batchJobSubmissionOp.currentApplicationState.isDefined)
+      assert(batchJobSubmissionOp.currentApplicationState.get.id == null)
       assert(batchJobSubmissionOp.getStatus.state === OperationState.ERROR)
     }
   }