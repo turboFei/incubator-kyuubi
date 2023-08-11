e6a37c8ee3a80f37d43b258cd579877504b1aa47
liangbowen <liangbowen@gf.com.cn>
2023-08-11 18:23:08 +0800

    [KYUUBI #5154] [Doc] Move configuration docs to the top level
    ### _Why are the changes needed?_
    
    - Move the configuration docs to the top level of docs, which is most commonly used and referenced
    - update relevant doc links
    
    ![image](https://github.com/apache/kyuubi/assets/1935105/a1dd35cc-d37f-4294-9fed-b275956c2cc5)
    
    ### _How was this patch tested?_
    - [x] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.readthedocs.io/en/master/contributing/code/testing.html#running-tests) locally before make a pull request
    
    Closes #5154 from bowenliang123/config-doc-first.
    
    Closes #5154
    
    b49ed3f8b [liangbowen] nit
    db7f0d14d [liangbowen] update doc links
    f8fd697a2 [liangbowen] move config docs to the top level
    7448e4487 [liangbowen] change title of settings doc
    40214ddd8 [liangbowen] move config doc in the front of deployment
    
    Authored-by: liangbowen <liangbowen@gf.com.cn>
    Signed-off-by: liangbowen <liangbowen@gf.com.cn>

--- kyuubi-server/src/test/scala/org/apache/kyuubi/config/AllKyuubiConfiguration.scala
+++ kyuubi-server/src/test/scala/org/apache/kyuubi/config/AllKyuubiConfiguration.scala
@@ -32,7 +32,7 @@ import org.apache.kyuubi.zookeeper.ZookeeperConf
 // scalastyle:off line.size.limit
 /**
  * End-to-end test cases for configuration doc file
- * The golden result file is "docs/deployment/settings.md".
+ * The golden result file is "docs/configuration/settings.md".
  *
  * To run the entire test suite:
  * {{{
@@ -47,7 +47,7 @@ import org.apache.kyuubi.zookeeper.ZookeeperConf
 // scalastyle:on line.size.limit
 class AllKyuubiConfiguration extends KyuubiFunSuite {
   private val kyuubiHome: String = Utils.getCodeSourceLocation(getClass).split("kyuubi-server")(0)
-  private val markdown = Paths.get(kyuubiHome, "docs", "deployment", "settings.md")
+  private val markdown = Paths.get(kyuubiHome, "docs", "configuration", "settings.md")
     .toAbsolutePath
 
   private def loadConfigs = Array(
@@ -67,7 +67,7 @@ class AllKyuubiConfiguration extends KyuubiFunSuite {
 
     builder ++=
       s"""
-         |# Introduction to the Kyuubi Configurations System
+         |# Configurations
          |
          |Kyuubi provides several ways to configure the system and corresponding engines.
          |
