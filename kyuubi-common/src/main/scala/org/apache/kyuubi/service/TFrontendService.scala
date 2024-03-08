1a0795036c5a8c058c782afb16ca15c2dceb8fc5
Cheng Pan <chengpan@apache.org>
2024-03-08 10:19:30 +0800

    [KYUUBI #6117] Bump Kyuubi Shaded 0.3.0
    This PR upgrades Kyuubi Shaded to 0.3.0, the major changes are:
    
    - upgrade Thrift from 0.9.3-1 to 0.16.0
    - upgrade JLine used by Zookeeper CLI from 0.9.94 to 2.14.6
    - split kyuubi-relocated-thrift from kyuubi-relocated-hive-service-rpc
    - introduce a light kyuubi-relocated-hive-metastore-client for getting DelegationToken
    
    Due to THRIFT-5297 (fixed in 0.14.0) removes requestTimeout and beBackoffSlotLength, the following Kyuubi configurations are removed too.
    
    - kyuubi.frontend.login.timeout
    - kyuubi.frontend.thrift.login.timeout
    - kyuubi.frontend.backoff.slot.length
    - kyuubi.frontend.thrift.backoff.slot.length
    
    Previously, Zookeeper 3.4 depends on JLine 0.9.94 while Zookeeper 3.6 and Hive Beeline depends on JLine 2.14.6, we pulls different versions of JLine into the binary distribution. Now we align them to a single JLine 2.14.6.
    
    Upgrade dependencies and change code to adapt to the breaking changes, also upgrade migration guide to mention the removed configurations.
    
    - [ ] Bugfix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    
    Pass GA.
    
    ---
    
    - [x] This patch was not authored or co-authored using [Generative Tooling](https://www.apache.org/legal/generative-tooling.html)
    
    **Be nice. Be informative.**
    
    Closes #6117 from pan3793/shaded-0.3.
    
    Closes #6117
    
    5639c9fd8 [Cheng Pan] nit
    d281cdb4b [Cheng Pan] fix
    184e1b95b [Cheng Pan] fix
    1628337c8 [Cheng Pan] remove unused conf
    24db2d5ad [Cheng Pan] try fix
    1e995bb34 [Cheng Pan] nit
    1d8e9bce9 [Cheng Pan] dep list
    7f33624e8 [Cheng Pan] Bump Kyuubi Shaded 0.3.0
    
    Authored-by: Cheng Pan <chengpan@apache.org>
    Signed-off-by: Cheng Pan <chengpan@apache.org>

--- kyuubi-common/src/main/scala/org/apache/kyuubi/service/TFrontendService.scala
+++ kyuubi-common/src/main/scala/org/apache/kyuubi/service/TFrontendService.scala
@@ -728,5 +728,9 @@ private[kyuubi] object TFrontendService {
     }
 
     def getSessionHandle: SessionHandle = sessionHandle
+
+    override def unwrap[T](aClass: Class[T]): T = null.asInstanceOf[T]
+
+    override def isWrapperFor(aClass: Class[_]): Boolean = false
   }
 }
