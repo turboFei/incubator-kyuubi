a45e1d41dccfae5806ca33bba6172b722ac7af61
Wang, Fei <fwang12@ebay.com>
2024-03-28 15:50:39 -0700

    users

--- kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/v1/EbayResource.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/v1/EbayResource.scala
@@ -17,8 +17,8 @@
 
 package org.apache.kyuubi.server.api.v1
 
-import javax.ws.rs.{GET, NotAllowedException, Path, Produces, QueryParam}
-import javax.ws.rs.core.MediaType
+import javax.ws.rs.{DELETE, GET, NotAllowedException, NotFoundException, Path, Produces, QueryParam}
+import javax.ws.rs.core.{MediaType, Response}
 
 import scala.collection.JavaConverters._
 
@@ -27,9 +27,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.apache.hadoop.security.UserGroupInformation
 
-import org.apache.kyuubi.{Logging, Utils}
-import org.apache.kyuubi.config.KyuubiConf
+import org.apache.kyuubi.{KYUUBI_VERSION, Logging, Utils}
+import org.apache.kyuubi.config.{KyuubiConf, KyuubiEbayConf}
+import org.apache.kyuubi.engine.{EngineType, ShareLevel}
+import org.apache.kyuubi.ha.HighAvailabilityConf
+import org.apache.kyuubi.ha.client.DiscoveryClientProvider.withDiscoveryClient
 import org.apache.kyuubi.server.api.ApiRequestContext
+import org.apache.kyuubi.session.KyuubiSessionManager
 
 @Tag(name = "Ebay")
 @Produces(Array(MediaType.APPLICATION_JSON))
@@ -37,8 +41,12 @@ private[v1] class EbayResource extends ApiRequestContext with Logging {
   private lazy val administrators = fe.getConf.get(KyuubiConf.SERVER_ADMINISTRATORS).toSet +
     Utils.currentUser
 
-  private def isAdministrator(userName: String): Boolean = {
-    administrators.contains(userName)
+  private def checkAdministrator(): Unit = {
+    val userName = fe.getSessionUser(Map.empty[String, String])
+    if (!administrators.contains(userName)) {
+      throw new NotAllowedException(
+        s"$userName is not allowed to list all live sessions")
+    }
   }
 
   @ApiResponse(
@@ -50,13 +58,92 @@ private[v1] class EbayResource extends ApiRequestContext with Logging {
   @GET
   @Path("groups")
   def groups(@QueryParam("user") user: String): Seq[String] = {
-    val userName = fe.getSessionUser(Map.empty[String, String])
-    val ipAddress = fe.getIpAddress
-    info(s"Received listing all live sessions request from $userName/$ipAddress")
-    if (!isAdministrator(userName)) {
-      throw new NotAllowedException(
-        s"$userName is not allowed to list all live sessions")
-    }
+    checkAdministrator()
     UserGroupInformation.createRemoteUser(user).getGroups.asScala
+  }
+
+  @ApiResponse(
+    responseCode = "200",
+    content = Array(new Content(
+      mediaType = MediaType.APPLICATION_JSON,
+      array = new ArraySchema(schema = new Schema(implementation = classOf[String])))),
+    description = "get the users")
+  @GET
+  @Path("users")
+  def sparkUsers(@QueryParam("cluster") cluster: String): Seq[(String, Seq[String])] = {
+    checkAdministrator()
+
+    val clusterOptList = Option(cluster).map(c => Seq(Option(c))).getOrElse {
+      KyuubiEbayConf.getNonCarmelClusterOptList(fe.getConf)
+    }
+
+    clusterOptList.map { clusterOpt =>
+      val clusterConf = getClusterConf(clusterOpt)
+      val users = withDiscoveryClient(clusterConf) { discoveryClient =>
+        val engineSpace = sparkSQLUserEngineSpace(clusterConf)
+        discoveryClient.getChildren(engineSpace)
+      }
+      s"${clusterOpt.orNull}" -> users
+    }
+  }
+
+  @ApiResponse(
+    responseCode = "200",
+    content = Array(new Content(
+      mediaType = MediaType.APPLICATION_JSON,
+      array = new ArraySchema(schema = new Schema(implementation = classOf[String])))),
+    description = "delete the user spark engines with specified subdomain")
+  @DELETE
+  @Path("engines")
+  def deleteUsersSparkEngine(
+      @QueryParam("cluster") cluster: String,
+      @QueryParam("users") users: Seq[String],
+      @QueryParam("subdomain") subdomain: String = "default"): Response = {
+    checkAdministrator()
+    val clusterOptList = Option(cluster).map(c => Seq(Option(c))).getOrElse {
+      KyuubiEbayConf.getNonCarmelClusterOptList(fe.getConf)
+    }
+
+    val responseMessageBuilder = new StringBuilder()
+    clusterOptList.foreach { clusterOpt =>
+      val clusterConf = getClusterConf(clusterOpt)
+      val engineSpace = sparkSQLUserEngineSpace(clusterConf)
+      withDiscoveryClient(clusterConf) { discoveryClient =>
+        users.foreach { user =>
+          val userEngineSpace = s"$engineSpace/$user/$subdomain"
+          try {
+            val engineNodes = discoveryClient.getChildren(userEngineSpace)
+            if (engineNodes.isEmpty) {
+              responseMessageBuilder.append(s"No engine found in $userEngineSpace\n")
+            } else {
+              engineNodes.foreach { engine =>
+                discoveryClient.delete(s"$userEngineSpace/$engine")
+                responseMessageBuilder.append(s"Deleted $engine from $userEngineSpace\n")
+              }
+            }
+          } catch {
+            case _: Exception =>
+              responseMessageBuilder.append(s"No engine found in $userEngineSpace\n")
+          }
+        }
+      }
+    }
+
+    Response.ok(responseMessageBuilder.toString()).build()
+  }
+
+  private def getClusterConf(clusterOpt: Option[String]): KyuubiConf = {
+    try {
+      fe.be.sessionManager.asInstanceOf[KyuubiSessionManager].getClusterConf(
+        clusterOpt.map(c => Map(KyuubiEbayConf.SESSION_CLUSTER.key -> c)).getOrElse(Map.empty))
+    } catch {
+      case _: Throwable =>
+        throw new NotFoundException(s"Please specify the correct cluster to access.")
+    }
+  }
+
+  private def sparkSQLUserEngineSpace(conf: KyuubiConf): String = {
+    val serverSpace = conf.get(HighAvailabilityConf.HA_NAMESPACE)
+    s"${serverSpace}_${KYUUBI_VERSION}_${ShareLevel.USER}_${EngineType.SPARK_SQL}"
   }
 }
